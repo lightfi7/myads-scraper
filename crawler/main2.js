bcadb85f9132c99aeeb438637294c8e6cd6d2f10
Fi <158217226+lightfi7@users.noreply.github.com>
2024-03-26 15:44:37 -0700

    Update main2.js

--- crawler/main2.js
+++ crawler/main2.js
@@ -126,6 +126,7 @@ const start = async () => {
     console.log(begin, end, n);
     const data = {
       authorization,
+      platform
       begin,
       end,
       n,
@@ -137,7 +138,7 @@ const start = async () => {
           const xhr = new XMLHttpRequest();
           xhr.open(
             "GET",
-            `https://bigspy.com/ecom/get-ecom-ads?favorite_app_flag=0&ecom_category=&search_type=1&platform=${platform}&category=&tag_ids=&ad_positions=&ads_ai_category=&video_duration_type=&os=0&ads_promote_type=0&geo=&exclude_geo=&audience_sex=&audience_age=&game_play=&game_style=&type=2&page=${data.n}&industry=3&language=&keyword=&sort_field=first_seen&region=&seen_begin=${data.begin}&seen_end=${data.end}&original_flag=1&is_preorder=&resume_or_new_ads=&is_real_person=0&theme=&text_md5=&ads_size=&size=60&ads_format=&exclude_keyword=&cod_flag=0&is_theater=0&is_ai_app=0&landing_page=0&cta_type=0&new_ads_flag=1&like_begin=&like_end=&comment_begin=&comment_end=&share_begin=&share_end=&position=0&is_hide_advertiser=0&advertiser_key=&dynamic=0&shopping=0&duplicate=0&software_types=&ecom_types=&social_account=&modules=fn&page_id=&landing_type=0&is_first=0&page_load_more=1&source_app=&redirect_filter_type=&iframe=fn`
+            `https://bigspy.com/ecom/get-ecom-ads?favorite_app_flag=0&ecom_category=&search_type=1&platform=${data.platform}&category=&tag_ids=&ad_positions=&ads_ai_category=&video_duration_type=&os=0&ads_promote_type=0&geo=&exclude_geo=&audience_sex=&audience_age=&game_play=&game_style=&type=2&page=${data.n}&industry=3&language=&keyword=&sort_field=first_seen&region=&seen_begin=${data.begin}&seen_end=${data.end}&original_flag=1&is_preorder=&resume_or_new_ads=&is_real_person=0&theme=&text_md5=&ads_size=&size=60&ads_format=&exclude_keyword=&cod_flag=0&is_theater=0&is_ai_app=0&landing_page=0&cta_type=0&new_ads_flag=1&like_begin=&like_end=&comment_begin=&comment_end=&share_begin=&share_end=&position=0&is_hide_advertiser=0&advertiser_key=&dynamic=0&shopping=0&duplicate=0&software_types=&ecom_types=&social_account=&modules=fn&page_id=&landing_type=0&is_first=0&page_load_more=1&source_app=&redirect_filter_type=&iframe=fn`
           );
           xhr.setRequestHeader("authorization", data.authorization);
           xhr.setRequestHeader("Referer", "Referer");
